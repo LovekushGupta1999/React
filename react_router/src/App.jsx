651123e48268655b7327e649d2c73404bfedd2be
lovekushgupta1999 <guptalovekush64@gmail.com>
2025-02-24 23:59:34 +0530

    route

--- react_router/src/App.jsx
+++ react_router/src/App.jsx
@@ -1,13 +1,23 @@
 import { useState } from 'react'
 import './App.css'
+import { Routes,Route } from 'react-router-dom'
+//pages
 import Navbar1  from './Components/Navbars'
+import About from './Components/About'
+import Contact from './Components/Contact'
 
 function App() {
 
 
   return (
     <>
+    <Routes>
+      <Route path='/Navbar1' element={<Navbar1/>}/>
+      <Route path='/About' element={<About/>}/>
+      <Route path='/Contact' element={<Contact/>}/>
     <Navbar1/>
+
+    </Routes>
     </>
   )
 }
