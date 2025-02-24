a03ea12d814b5113ae5e653248e327fe43207764
lovekushgupta1999 <guptalovekush64@gmail.com>
2025-02-24 23:31:09 +0530

    react-router

---
+++ react_router/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+import Navbar1  from './Components/Navbars'
+
+function App() {
+
+
+  return (
+    <>
+    <Navbar1/>
+    </>
+  )
+}
+
+export default App
