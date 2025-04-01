03bf836e7f5a386d24c0d76a27d46c8f1a479d41
lovekushgupta1999 <guptalovekush64@gmail.com>
2025-04-01 22:17:38 +0530

    Navbar

--- Ecommercewebsitereact/src/Components/Header/Header.jsx
+++ Ecommercewebsitereact/src/Components/Header/Header.jsx
@@ -6,6 +6,10 @@ import Select from '../SelectDrop/Select'
 import { useState } from 'react' 
 import axios from 'axios'
 import { LocationOnOutlined} from '@mui/icons-material'
+import Iconcompare from '../../assets/images/icon-compare.svg'
+import IconHeart from '../../assets/images/icon-heart.svg'
+import IconCart from '../../assets/images/icon-cart.svg'
+import IconUser from '../../assets/images/icon-user.svg'
 
 function Header() {
     const [Categories, setCategories]=useState([
@@ -29,7 +33,7 @@ function Header() {
             if(res!=null){
                 // console.log(res.data.data)
                 res.data.data.map((item)=>{
-                    console.log(item.country)
+                    // console.log(item.country)
                     countrylist.push(item.country)
 
                 })
@@ -63,6 +67,32 @@ function Header() {
                     <div className='selectCountry'>
                    <Select data={countrylist} placeholder= {'Your location'} icon={<LocationOnOutlined style={{Opacity:'0.3',fontSize:'17px'}}/> } />
                    </div>
+
+                   <ul className='list list-inline mb-0 headerTabs'>
+                    <li className='list-inline-item'>
+                      <span>
+                        <img src={Iconcompare} alt="" />
+                        <span className='badge bg-success rounded-circle'>1</span>
+                        Compare </span>
+                    </li>
+                    <li className='list-inline-item'>
+                      <span>
+                        <img src={IconHeart} alt="" />
+                        <span className='badge bg-success rounded-circle'>3</span>
+                        Whislist </span>
+                    </li>
+                    <li className='list-inline-item'>
+                      <span>
+                        <img src={IconCart} alt="" />
+                        <span className='badge bg-success rounded-circle'>2</span>
+                        Cart </span>
+                    </li>
+                    <li className='list-inline-item'>
+                      <span>
+                        <img src={IconUser} alt="" />
+                        Account </span>
+                    </li>
+                   </ul>
                 </div>
             </div>
         </div>
--- Ecommercewebsitereact/src/Components/SelectDrop/Select.jsx
+++ Ecommercewebsitereact/src/Components/SelectDrop/Select.jsx
@@ -11,26 +11,39 @@ function Select({data,placeholder,icon}) {
     const [Selectedindex,setSelectedindex]=useState(0)
     const [Selecteditem,setSelecteditem]=useState(placeholder)
 
+    const [listdata,setlistdata]=useState(data)
+    const [listdata2,setlistdata2]=useState(data)
     function CloseSelect(index,cate){
         setSelecteditem(cate)
         setSelectedindex(index)
         setisOpenSelect(!isOpenSelect)
     }
-    
+    const filterList=(e)=>{
+       const keyword=e.target.value.toLowerCase()
+       let n=keyword.length;
+       const list=listdata2.filter((item)=>{
+        return item.slice(0,n).toLowerCase().includes(keyword)
+       })
+       const list2=list.filter((item,index)=>{
+            return list.indexOf(item)===index
+       })
+       setlistdata(list2)
+    }
     return (
         <> <ClickAwayListener onClickAway={()=>setisOpenSelect(false)}>
-           <div className='selecrDrop cursor position-relative'>
+           <div className='selectDropwrapper cursor position-relative'>
             {icon}
             <span className='openSelect' onClick={()=>{setisOpenSelect(!isOpenSelect)}}> {Selecteditem} <KeyboardArrowDown className='arrow'/></span>
             { isOpenSelect===true &&
         <div className='selectDrop'>
             <div className='searchfield'>
-                <input type="text"  placeholder='Search here'/>
+                <input type="text"  placeholder='Search here...'onChange={filterList}/>
+              
             </div>
 
             <ul className='searchResults'>
                 {
-                    data.map((item,index)=>{
+                    listdata.map((item,index)=>{
                         return(
                                 <li onClick={()=>CloseSelect(index,item)} className={`${Selectedindex=== 0? 'active' : '' }`}>{item}</li>
                               )     } )
